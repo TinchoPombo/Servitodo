f73429871342e5412cfff5981755da0814b9713c
TomasBe24 <tomas.berias1@hotmail.com>
2022-10-25 19:22:04 -0300

    Coment TipoEstado

--- Servitodo/app/src/main/java/com/ort/servitodo/entities/TipoEstado.kt
+++ Servitodo/app/src/main/java/com/ort/servitodo/entities/TipoEstado.kt
@@ -2,4 +2,9 @@ package com.ort.servitodo.entities
 
 enum class TipoEstado {
     EN_CURSO, PENDIENTE, RECHAZADO, APROBADO, RESERVADO, FINALIZADO
+
+    //Pendiente: el cliente pidio el servicio, pero todavia no fue aceptado por el prestador
+    //Reservado: el prestador acepta el servicio, se habilita wp, y se puede modificar la fecha
+    //Aprobado: el prestador acepta el servicio definitivamente, con el horario definido.
+
 }
