7b46c328d337dee5b70e987a017eaa621534dd1e
RobertinoMomo15 <85981493+RobertinoMomo15@users.noreply.github.com>
2022-11-08 00:20:16 -0300

    estetica filtros

--- Servitodo/app/src/main/java/com/ort/servitodo/fragments/cliente/HomeClienteFragment.kt
+++ Servitodo/app/src/main/java/com/ort/servitodo/fragments/cliente/HomeClienteFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
@@ -46,6 +47,22 @@ class HomeClienteFragment : Fragment() {
         homeClienteViewModel.setView(v)
 
         return v
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val rubros = resources.getStringArray(R.array.rubros)
+        val filtroDistancia = resources.getStringArray(R.array.filtroDistancia)
+        val filtroPuntuacion = resources.getStringArray(R.array.filtroPuntuacion)
+
+        val arrayAdapterRubros = ArrayAdapter(requireContext(), R.layout.dropdown_item, rubros)
+        val arrayAdapterDistancia = ArrayAdapter(requireContext(), R.layout.dropdown_item, filtroDistancia)
+        val arrayAdapterPuntuacion = ArrayAdapter(requireContext(), R.layout.dropdown_item, filtroPuntuacion)
+
+        binding.autoCompleteTextView1.setAdapter(arrayAdapterPuntuacion)
+        binding.autoCompleteTextView2.setAdapter(arrayAdapterRubros)
+        binding.autoCompleteTextView3.setAdapter(arrayAdapterDistancia)
+
     }
 
     override fun onStart() {
