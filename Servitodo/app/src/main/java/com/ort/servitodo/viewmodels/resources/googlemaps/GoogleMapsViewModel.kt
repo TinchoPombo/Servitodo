d65df40fa1b55e477b653cb43129085ffb5dd62f
dannybak <danielbak10@gmail.com>
2022-11-10 16:33:29 -0300

    Agregando info marker y redireccionamiento desde maps a detalle publicacion

--- Servitodo/app/src/main/java/com/ort/servitodo/viewmodels/resources/googlemaps/GoogleMapsViewModel.kt
+++ Servitodo/app/src/main/java/com/ort/servitodo/viewmodels/resources/googlemaps/GoogleMapsViewModel.kt
@@ -1,116 +1,161 @@
 package com.ort.servitodo.viewmodels.resources.googlemaps
 
 import android.content.Context
-import android.content.Intent
 import android.location.Address
 import android.location.Geocoder
-import android.net.Uri
+import android.text.Editable
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.google.maps.android.SphericalUtil
 import com.ort.servitodo.R
+import com.ort.servitodo.entities.Publicacion
 import com.ort.servitodo.entities.Usuario
+import com.ort.servitodo.fragments.cliente.DetallePublicacionFragment
+import com.ort.servitodo.fragments.cliente.DetallePublicacionFragmentArgs
+import com.ort.servitodo.repositories.PublicacionRepository
 import com.ort.servitodo.repositories.UsuarioRepository
 import kotlinx.coroutines.launch
 
 class GoogleMapsViewModel : ViewModel() , OnMapReadyCallback {
 
+    private val ARGENTINA_LATLNG_BOUNDS = LatLngBounds(
+        LatLng(-56.9, -78.95), LatLng(-22.1, -51.9))
+
     private lateinit var view : View
     private lateinit var currentUser : Usuario
     private lateinit var supportMapFragment : SupportMapFragment
     private lateinit var usuarioRepository : UsuarioRepository
+    private lateinit var markerAdapter : MarkerInfoWindowAdapter
 
     //--> Mutable Live Data
-    val selectedDir = MutableLiveData<String>()
     val cargando = MutableLiveData<String>()
     val kmFilter = MutableLiveData<Int>()
 
     //--> Lists
     var addressList : MutableList<Address> = mutableListOf()
     var usersList : MutableList<Usuario> = mutableListOf()
+    var publicacionesList : MutableList<Publicacion> = mutableListOf()
 
     //------------------------------------------------------------------------------------
-    fun setView(v : View){
+    private fun setView(v : View){
         this.view = v
-        this.usuarioRepository = UsuarioRepository(v)
+        markerAdapter = MarkerInfoWindowAdapter(view.context)
     }
 
-    fun initUsers(){
-        viewModelScope.launch {
-            val id = usuarioRepository.getIdSession()
-            currentUser = usuarioRepository.getUsuarioById(id)
+    private suspend fun initUsers(){
+        val id = usuarioRepository.getIdSession()
+        currentUser = usuarioRepository.getUsuarioById(id) //--> Init usuario actual (logueado)
 
-            val lista = if(currentUser.esPrestador){
-                usuarioRepository.getClientes()
-            } else{
-                usuarioRepository.getPrestadores()
-            }
-            usersList = lista.filter { u -> u.id != currentUser.id } as MutableList<Usuario>
+        val lista = if(currentUser.esPrestador){
+            usuarioRepository.getClientes()
+        } else{
+            usuarioRepository.getPrestadores()
         }
+        usersList = lista.filter { u -> u.id != currentUser.id } as MutableList<Usuario> //--> Init lista
     }
 
-    fun getCurrentUserMarker(mMap : GoogleMap) : Marker?{
-        val u = this.currentUser
-        val dir = convertStringToLatLng("${u.ubicacion}, CABA")
-        val marker = mMap.addMarker(MarkerOptions().position(dir).title("Mi casa").icon(
-                BitmapDescriptorFactory.defaultMarker
-                (BitmapDescriptorFactory.HUE_BLUE)))
-        return marker
+    private fun validateCurrentUserAddress() : Boolean{
+        val address = getAddress(this.currentUser.ubicacion)
+        return address.size > 0
+    }
+
+    //------------------------------------------------------------------------------------
+    fun initMap(f : Fragment, v : View){
+        viewModelScope.launch {
+            setView(v)
+            usuarioRepository = UsuarioRepository(v)
+            initUsers()
+            publicacionesList = PublicacionRepository().getPublicaciones()
+            if(kmFilter.value == null){
+                createGoogleMaps(f, "0km")
+            }
+        }
     }
 
     //------------------------------------------------------------------------------------
     //--> Crea el google maps en el fragment pasado por params
-    fun createGoogleMaps(f : Fragment, km : Int){
-        this.kmFilter.value = km
-        cargando.value = "Cargando..."
+    fun createGoogleMaps(f : Fragment, km : String){
+        if(!validateCurrentUserAddress()){
+            popUpInvalidAddress()
+        }
+        else if(km.isNullOrEmpty()){
+            Snackbar.make(view, "Debes elegir una opcion", Snackbar.LENGTH_SHORT).show()
+        }
+        else{
+            this.kmFilter.value = getNumberFromRadioButton(km)
+            cargando.value = "Cargando..."
 
-        if(!f.isAdded) return;
-        supportMapFragment = f.childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        supportMapFragment.getMapAsync(this)
+            if(!f.isAdded) return;
+            supportMapFragment = f.childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            supportMapFragment.getMapAsync(this)
+        }
     }
 
     //--> Callback del getMapAsync
     override fun onMapReady(googleMap: GoogleMap) {
 
-        viewModelScope.launch {
-            val mMap = googleMap
-            val listOfMarker = ArrayList<Marker>()
+        googleMap.setLatLngBoundsForCameraTarget(ARGENTINA_LATLNG_BOUNDS);
+        googleMap.setMinZoomPreference(10F)
 
-            mMap.clear() //--> borra viejos marcadores
+        googleMap.setInfoWindowAdapter(markerAdapter)
+        googleMap.clear() //--> borra viejos marcadores
 
-            for(u in usersList){
-                val dir = convertStringToLatLng("${u.ubicacion}, CABA")
-                val distance = calculateKm(dir)
-                if(distance <= kmFilter.value!!){
-                    val dist = String.format("%.2f", distance / 1000)
-                    val marker = mMap.addMarker(MarkerOptions().position(dir).title("${u.nombre} ${u.apellido} (${dist}km)"))
-                    listOfMarker.add(marker!!)
-                }
+        val listOfMarker = ArrayList<Marker>()
+        listOfMarker.add(getCurrentUserMarker(googleMap))
+
+        for (u in usersList) {
+            val dir = convertStringToLatLng("${u.ubicacion}, CABA")
+            val distance = calculateKm(dir)
+            val publicacion = getPublicacionFromUser(u)
+            if (publicacion != null && distance <= kmFilter.value!!) {
+                val marker = googleMap.addMarker(
+                    MarkerOptions()
+                        .position(dir)
+                        .title("${u.nombre} ${u.apellido} (${publicacion.rubro.nombre})")
+                        .snippet("${String.format("%.2f", distance)}km")
+                )
+                marker!!.tag = publicacion
+
+                marker.showInfoWindow()
+                marker.hideInfoWindow()
+
+                listOfMarker.add(marker)
+
+                redirectToDetallePublicacion(googleMap)
             }
-            listOfMarker.add(getCurrentUserMarker(googleMap)!!)
-            showAllMarkers(mMap, listOfMarker)
-            cargando.value = ""
         }
+        showAllMarkers(googleMap, listOfMarker)
+
+        cargando.value = if(listOfMarker.size == 1 && this.kmFilter.value!! >= 1){
+             "No hay prestadores a ${this.kmFilter.value}km"
+        } else{ "" }
     }
 
     //--> Redireccionamiento a Google Maps
-    fun redirectToGoogleMaps(){
-        val gmmIntentUri = Uri.parse("https://www.google.co.in/maps/dir/${currentUser.ubicacion}/${selectedDir.value}")
+    /*fun redirectToGoogleMaps(goto : String){
+        val gmmIntentUri = Uri.parse("https://www.google.co.in/maps/dir/${currentUser.ubicacion}/${goto}")
         val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
         mapIntent.setPackage("com.google.android.apps.maps")
         view.context.startActivity(mapIntent)
-    }
+    }*/
 
     //--------------------------------- FUNCIONES PRIVADAS -----------------------------------------------
     //--> Muestra los marcadores
@@ -119,23 +164,22 @@ class GoogleMapsViewModel : ViewModel() , OnMapReadyCallback {
         for (m in listOfMarker) {
             b.include(m.position)
         }
-        val cu = CameraUpdateFactory.newLatLngBounds(b.build(),100)  // 1° param: bounds, 2° param: paddingFromEdgeAsPX
-        mMap.animateCamera(cu)
+        if(listOfMarker.size > 1){
+            val cu = CameraUpdateFactory.newLatLngBounds(b.build(),100)  // 1° param: bounds, 2° param: paddingFromEdgeAsPX
+            mMap.animateCamera(cu)
+        }else{
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(listOfMarker[0].position, 18.0f))
+        }
     }
-
 
     //--> Convierte la direccion (string) a coordenadas
     private fun convertStringToLatLng(dir : String) : LatLng{
-        var lat = 0.0
-        var lng = 0.0
-
         addressList = this.getAddress(dir)
-        lat = addressList[0].latitude
-        lng = addressList[0].longitude
-        return LatLng(lat, lng)
+        return LatLng(addressList[0].latitude, addressList[0].longitude)
     }
 
 
+    //--> Devuelve una lista con la direccion encontrada (el limite es 1 resultado)
     private fun getAddress(dir : String) : MutableList<Address>{
         val geocoder : Geocoder = Geocoder(view.context)
         return geocoder.getFromLocationName(dir, 1)
@@ -143,25 +187,57 @@ class GoogleMapsViewModel : ViewModel() , OnMapReadyCallback {
 
 
     //--> Calcula la distancia en km entre dos puntos
-    private fun calculateKm(dir : LatLng) : Double{
+    private fun calculateKm(dir: LatLng): Double {
         val currentUserDir = this.convertStringToLatLng(this.currentUser.ubicacion)
-        val result = SphericalUtil.computeDistanceBetween(currentUserDir, dir);
-        return result / 1000
+        return SphericalUtil.computeDistanceBetween(currentUserDir, dir) / 1000
     }
 
 
-    //--> Obtiene el numero del valor del radiobutton
-    fun getNumberFromRadioButton(value : String) : Int{
+    //--> Obtiene el numero del valor del radiobutton (index en 0 trae el num en string)
+    private fun getNumberFromRadioButton(value : String) : Int{
         val strs = value.split("km").toTypedArray()
         return strs[0].toInt()
     }
 
-    //----------------------------------- OTROS ---------------------------------------------
-    //--> Esconde el keyboard
-    private fun View.hideKeyboard() {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(windowToken, 0)
+    //--> Obtiene el marker del usuario actual (logueado)
+    private fun getCurrentUserMarker(mMap : GoogleMap) : Marker {
+        val dir = convertStringToLatLng("${this.currentUser.ubicacion}, CABA")
+        val icon: BitmapDescriptor by lazy {
+            val color = ContextCompat.getColor(view.context, R.color.googlemaps_marker)
+            BitmapHelper.vectorToBitmap(view.context, R.drawable.home, color)
+        }
+        val marker = mMap.addMarker(MarkerOptions().position(dir).title("Mi casa").icon(icon))
+        marker!!.tag = marker
+        marker.showInfoWindow()
+        return marker
     }
 
+    private fun getPublicacionFromUser(u : Usuario) : Publicacion?{
+        return this.publicacionesList.find{ p -> p.idPrestador == u.id }
+    }
+
+    private fun redirectToDetallePublicacion(mMap : GoogleMap){
+        mMap.setOnInfoWindowClickListener {
+            val publicacion = it.tag
+            val bundle = bundleOf("receivePublicacion" to publicacion)
+            view.findNavController().navigate(R.id.detallePublicacionFragment, bundle)
+        }
+    }
+
+    //----------------------------------- OTROS ---------------------------------------------
+    //--> Esconde el keyboard
+    /*private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }*/
     //Para llamar al hideKeyborad() --> view.hideKeyboard()
+
+    private fun popUpInvalidAddress(){
+        MaterialAlertDialogBuilder(view.context)
+            .setTitle("Direccion invalida")
+            .setMessage("Debes tener una direccion valida")
+            .setPositiveButton("Ok") { dialog, which ->
+            }
+            .show()
+    }
 }
---
+++ Servitodo/app/src/main/java/com/ort/servitodo/viewmodels/resources/googlemaps/MarkerInfoWindowAdapter.kt
@@ -0,0 +1,98 @@
+package com.ort.servitodo.viewmodels.resources.googlemaps
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.Nullable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.ort.servitodo.R
+import com.ort.servitodo.entities.Publicacion
+import javax.sql.DataSource
+
+class MarkerInfoWindowAdapter(private val context: Context) : GoogleMap.InfoWindowAdapter {
+
+    //*** Vista que se utilizará para toda la ventana de información ***
+    //Si esta devuelve null, entonces se ejecuta el 2° metodo getInfoContents
+    override fun getInfoWindow(marker: Marker): View? {
+        return null
+    }
+
+    //*** Permite personalizar contenido de la ventana (info, marco, fondo, etc) ***
+    //--> Si esta devuelve null, entonces se muestra la ventana de información predeterminada
+    @SuppressLint("InflateParams", "SetTextI18n")
+    override fun getInfoContents(marker: Marker): View? {
+
+        return if(marker.title != "Mi casa"){
+            val publicacion : Publicacion = marker.tag as Publicacion
+            val view = LayoutInflater.from(context).inflate(R.layout.marker_info_contents, null)
+
+            val imageView = view.findViewById<ImageView>(R.id.markerinfoimg)
+            setImage(marker, publicacion.fotoPrestador, imageView)
+
+            view.findViewById<TextView>(R.id.text_view_title).text = "${publicacion.nombrePrestador} ${publicacion.apellidoPrestador}"
+            view.findViewById<TextView>(R.id.text_view_rubro).text = publicacion.rubro.nombre.uppercase()
+            view.findViewById<TextView>(R.id.text_view_km).text = marker.snippet
+            view.findViewById<TextView>(R.id.text_view_rating).text = "Puntuacion: "
+
+            view.findViewById<Button>(R.id.markerinfobutton).setOnClickListener{
+                //Log.d("aaasasasasasasasasasasasasa", "Hola soy el boton en el markerinfo")
+            }
+
+            view
+        } else null
+    }
+
+
+    //--------------------------------------- GLIDE ----------------------------------------------
+    private val images: HashMap<Marker, Bitmap> = HashMap()
+    private val targets: HashMap<Marker, CustomTarget<Bitmap>> = HashMap()
+
+    //---> create this inner class
+    inner class InfoTarget(var marker: Marker) : CustomTarget<Bitmap>() {
+        override fun onLoadCleared(@Nullable placeholder: Drawable?) {
+            //images.remove(marker)
+        }
+
+        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+            images.put(marker, resource)
+            //marker.showInfoWindow()
+        }
+    }
+
+    //---> Methods
+    private fun getTarget(marker: Marker): CustomTarget<Bitmap> {
+        var target = targets[marker]
+        if (target == null) {
+            target = InfoTarget(marker)
+            targets[marker] = target
+        }
+        return target
+    }
+
+    fun setImage(marker : Marker, foto : String, imageView: ImageView){
+        val image = images[marker]
+        if (image == null) {
+            Glide.with(context)
+                .asBitmap()
+                .load(foto)
+                .dontAnimate()
+                .into(getTarget(marker))
+        } else {
+            imageView.setImageBitmap(image)
+        }
+    }
+
+}
